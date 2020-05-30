1169f4ca836c77845ad3d02437df95453cc6599e
sagarPakhrin <sagarlama826@gmail.com>
2020-05-31 01:32:48 +0545

    Updates

---
+++ precache-manifest.f33b1b513eee3a2eb7d54e9bee3b15af.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77e03549ecb9b0e7368b64186460056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9cebf0297ffadbab939",
+    "url": "/static/css/main.e9a5b6cf.chunk.css"
+  },
+  {
+    "revision": "8a1a39f25c0c9bd4cd07",
+    "url": "/static/js/2.e33880cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e33880cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cebf0297ffadbab939",
+    "url": "/static/js/main.5f2faca4.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "9807576fe7a4bdd0ed73b36a4510938f",
+    "url": "/static/media/django.9807576f.svg"
+  },
+  {
+    "revision": "2e0fcd7b793e8353f3e272dc38492020",
+    "url": "/static/media/docker.2e0fcd7b.svg"
+  },
+  {
+    "revision": "0198b80cc41d8c623b32e292fecc1b29",
+    "url": "/static/media/downloadInverse.0198b80c.svg"
+  },
+  {
+    "revision": "f45070297b2a1fc9976f47e463ff3d7e",
+    "url": "/static/media/ecommerceThumbnail.f4507029.png"
+  },
+  {
+    "revision": "d382e3c7b60434ed1e0069f6cd1bdccd",
+    "url": "/static/media/githubInverse.d382e3c7.svg"
+  },
+  {
+    "revision": "891a71b9a98f1d58491e1b06c4d356a2",
+    "url": "/static/media/js.891a71b9.svg"
+  },
+  {
+    "revision": "1d56556118bdc8c2a0f4e945dc0b9401",
+    "url": "/static/media/linkedinInverse.1d565561.svg"
+  },
+  {
+    "revision": "be2df79601b32143d336bd076429b909",
+    "url": "/static/media/nodejs.be2df796.svg"
+  },
+  {
+    "revision": "d956cbbb2e59777272370c854a3b6747",
+    "url": "/static/media/php.d956cbbb.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "0e1090970fb9c533f20c92b8dea76934",
+    "url": "/static/media/socialThumbnail.0e109097.png"
+  },
+  {
+    "revision": "da30b99fc0bad5aaa92b8f8a8f8c4d31",
+    "url": "/static/media/twitter_temp.da30b99f.svg"
+  },
+  {
+    "revision": "6accb19f013248eeb06e412d3dbea727",
+    "url": "/static/media/vim.6accb19f.svg"
+  }
+]);
